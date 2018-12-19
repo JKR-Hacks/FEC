a9c526dcc89440c20df95c80e300db76c972b28c
Joe Boulos <jab12288@gmail.com>
2018-12-18 19:15:39 -0800

    rendering data dynamically from sample data

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -7,7 +7,15 @@ var Schedule = (props) => (
       {props.teamData.map((team) => {
         return(
           <li className='team-list-item'key={team.id}>
-            <div className='team-list-item-teamname'>{team.team}</div>
+            <div className='team-list-item-teamname'>TEAM NAME: @{team.team}</div>
+            <div>NEWS FEED: {team.feed[0]}</div>
+            <div>HIGH PASS: {team.players[1]} {team.stats.pass[0]} yards</div>
+            <div>HIGH REC: {team.players[2]} {team.stats.rec[0]} yards</div>
+            <div>HIGH RUSH: {team.players[3]} {team.stats.rush[0]} yards</div>
+            <div>TEAM ROSTER: {team.players[0]}, etc...</div>
+            <div>WIN-LOSS: {team.winloss[0]}</div>
+            <div>OPPONENTS: {team.opponent[0]}</div>
+            <div>WEEK: {team.week[0]}</div>
           </li>
         )
       })}
